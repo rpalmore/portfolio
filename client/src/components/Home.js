e97c5376e6dfe8f73c11d742584ce1fa776ced9c
rebecca palmore <rkpalmore@gmail.com>
2017-10-25 22:34:33 -0500

    header

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,18 +7,38 @@ import {
 import './Home.css';
 // import Welcome from '../containers/Welcome.js'
 
+import $ from "jquery";
+window.jQuery = window.$ = $;
+require('textillate');
+require('letteringjs');
+require('animate.css');
+
+
+$(document).ready(function(){
+ console.log("Ready!");
+  $(function () {
+  	$('.tltHome').textillate({ 
+  		in: { 
+  			effect: 'flash',  
+  			shuffle: true 
+  		} 
+  	});
+  })
+});
+
 class Home extends Component {
   render() {
     return (
       <div className='container'>
+       <div className='containerLayer'></div>
        <div className='headerBackground'>
         <div className='headerImage'>
-        <div className='headerLayer'>
-      	 <div className='header'>
-      	  <Link className='nameLink' to='/home'>Rebecca Palmore</Link>
+         <div className='headerLayer'>
+      	  <div className='header'>
+      	   <Link className='tltHome nameLink' to='/home'>Rebecca Palmore</Link>
+      	  </div>
       	 </div>
-      	 </div>
-        </div>
+      	</div>
        </div>
       </div>
 	);
