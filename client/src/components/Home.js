061f340e30d14a8b4f2db86cf351b9b5e668d3ee
rebecca palmore <rkpalmore@gmail.com>
2017-12-05 11:45:28 -0600

    adding social links

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -11,6 +11,9 @@ import Projects from './Projects.js';
 import $ from "jquery";
 import {TweenMax, Power4, TweenLite} from "gsap";
 import ScrollToPlugin from "gsap/ScrollToPlugin";
+import github from "../images/logos/github.png";
+import twitter from "../images/logos/twitter.png";
+import linkedIn from "../images/logos/linkedIn.png";
 
 window.jQuery = window.$ = $;
 require('textillate');
@@ -29,9 +32,8 @@ $(document).ready(function(){
   })
 });
 
-
 class Home extends Component {
-
+  
   handleClick() {
       TweenLite.to(window, 1.5, {scrollTo: {y:'.mainContainer', offsetY:10}, ease:Power4.easeOut});
   }
@@ -39,38 +41,49 @@ class Home extends Component {
   render() {
     return (
     <Router>
-   	<div>
-      <div className='container'>
-        <div className='containerLayer'></div>
-        <div className='opaque'></div>
-        <div className='headerBackground'>
-          <div className='headerImage'>
-            <div className='headerLayer'></div>
-      	    <div className='header'>
-      	      <Link className='tltHome nameLink' to='/home'>rebecca palmore</Link>
+   	  <div>
+        <div className='container'>
+          <div className='containerLayer'></div>
+          <div className='opaque'></div>
+          <div className='headerBackground'>
+            <div className='headerImage'>
+              <div className='headerLayer'></div>
+      	      <div className='header'>
+      	        <Link className='tltHome nameLink' to='/home'>
+                rebecca palmore
+                </Link>
+                <div className='socialLinks'>
+                  <Link className='social1' to='https://github.com/rpalmore' target='_blank'><img className='socialIcons' src={github} alt={"Github"}/>
+                  </Link>
+                  <Link className='social2' to='https://twitter.com/rebeccapalmore' target='_blank'><img className='socialIcons' src={twitter} alt={"Twitter"}/>
+                  </Link>
+                  <Link className='social3' to='https://linkedin.com/in/rebecca-palmore' target='_blank'><img className='socialIcons' src={linkedIn} alt={"LinkedIn"}/>
+                  </Link>
+                </div>
+      	      </div>
       	    </div>
-      	  </div>
+          </div>
+        </div>
+
+        <div className='menuContainer'></div>
+        <div className='imageContainer'>
+	        <Link onClick={this.handleClick} className='nav' to='/about'>
+		        <div className='about'>about</div>
+          </Link>
+		      <Link onClick={this.handleClick} className='nav' to='/projects'>
+		        <div className='projects'>projects</div>
+	        </Link>
+	        <Link onClick={this.handleClick} className='nav' to='/contact'>
+	          <div className='contact'>contact</div>
+	        </Link>
+	      </div>
+
+	      <div className='mainContainer'>
+		      <Route path="/about" component={About}/>
+		      <Route path="/projects" component={Projects}/>
+	        <Route path="/contact" component={Contact}/>
         </div>
       </div>
-
-      <div className='menuContainer'></div>
-      <div className='imageContainer'>
-	    <Link onClick={this.handleClick} className='nav' to='/about'>
-		  <div className='about'>about</div></Link>
-		<Link onClick={this.handleClick} className='nav' to='/projects'>
-		  <div className='projects'>projects</div>
-	    </Link>
-	    <Link onClick={this.handleClick} className='nav' to='/contact'>
-	      <div className='contact'>contact</div>
-	    </Link>
-	  </div>
-
-	  <div className='mainContainer'>
-		  <Route path="/about" component={About}/>
-		  <Route path="/projects" component={Projects}/>
-	    <Route path="/contact" component={Contact}/>
-    </div>
-    </div>
     </Router>
 	);
   }
--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-// import './Projects.css';
+import './Projects.css';
 import Navbar from './Navbar.js';
 
 
 class Projects extends Component {
   render() {
     return (
-      <div>
-      	
-	      <p><b><a href="/RebeccaPalmore_Resume.pdf" download>Download my resume</a></b></p>
-	      <p>Hi! I’m Rebecca. Thanks for checking out my portfolio. I’ve been a full-stack web developer since 2017, and a multimedia journalist and editor since 2002.</p>
+      <div className='sectionContainer'>
+        <div className='sectionContainerInner'>
+          <div className='sectionImage'></div>
+          <div className='sectionImageLayerFull'></div>
+          <div className='sectionImageLayerPartial'></div>
+          <div className='textContainer'>
+      	    <div className='text'>
+              <p>Hello! Thanks for visiting. I’ve been a full-stack web developer since 2017, and a multimedia journalist and editor since 2002.</p>
+            </div>
 
-	      <p>Fast-paced newsrooms unwittingly prepared me for web development, where focus, intense attention to detail and quick problem-solving skills are essential, and a curious mind is what powers that need to look under the hood to try and answer a question that dogs so many of us: <i>How does it work?</i></p>
-
-	      <p>As a recent graduate of <a href="https://codingbootcamp.northwestern.edu/" target="_blank" rel="noopener noreferrer">Northwestern University’s Coding Bootcamp</a>, I’ve been immersed in learning how things work, from front-end design to server-side logic. Working with React, Express, JavaScript and Node.js&mdash;among other techonologies&mdash;has been hugely challenging, but with each project or in-class activity, there is immense satisfaction. I’m excited to build on my skills, and continue to refine class projects now that the bootcamp has ended.</p>
-
-	      <p>As a digital editor at <a href="http://wttw.com/chicagotonight" target="_blank" rel="noopener noreferrer">Chicago Tonight at WTTW-Channel 11</a>, I manage a small, driven team of reporters, produce compelling stories for digital platforms, edit and program a news website and&mdash;as many in my field do at all hours of the day and night&mdash;obsess over metrics.</p>
-
-	      <p>Here, you’ll find class projects and homework from the 24-week full-stack web development program at Northwestern. I'll add new projects as I build them.</p>
-	      <Navbar />
-      </div>
+	       <Navbar />
+          </div>
+	     </div>
+	    </div>
     );
   }
 }
