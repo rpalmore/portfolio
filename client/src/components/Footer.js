7c146e6ae29ee3b3e9b398538dae2df33bb416ef
rebecca palmore <rkpalmore@gmail.com>
2017-12-17 15:59:37 -0600

    footer enhancements

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,8 +7,9 @@ import github from "../images/logos/github.png";
 
 class Footer extends Component {
   render() {
-    return (         
+    return (  
       <footer>
+        <div className='footerLayer'></div>
         <Link to='https://github.com/rpalmore/portfolio' target='_blank'>
           <img className='portfolioGithub' src={github} alt={"Github"}/>
         </Link>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -36,59 +36,67 @@ $(document).ready(function(){
 class Home extends Component {
 
   handleClick = () => {
-     TweenLite.to(window, 1, {scrollTo: '.mainContainer', autokill: false, ease:Power4.easeOut});
+     TweenLite.to(window, 1, 
+      {scrollTo: {y:'.mainContainer'}, 
+       autokill: false, 
+       ease:Power4.easeOut
+     });
   }
 
   render() {
     return (
-    <Router>
-     <div>
-   	  <div className='wrapper'>
-          <div className='containerLayer'></div>
-          <div className='opaque'></div>
-          <div className='headerBackground'>
-            <div className='headerImage'>
+      <Router>
+        <div>
+   	      <div className='wrapper'>
+            <div className='containerLayer'></div>
+            <div className='opaque'></div>
+            <div className='headerBackground'>
+              <div className='headerImage'>
               <div className='headerLayer'></div>
-      	      <div className='header'>
-      	        <Link className='tltHome nameLink' to='/home'>
-                rebecca palmore
-                </Link>
-                <div className='socialLinks'>
-                  <Link to='https://github.com/rpalmore' target='_blank'><img className='socialIcons' src={github} alt={"Github"}/>
+      	        <div className='header'>
+      	          <Link className='tltHome nameLink' to='/home'>
+                    rebecca palmore
                   </Link>
-                  <Link to='https://twitter.com/rebeccapalmore' target='_blank'><img className='socialIcons' src={twitter} alt={"Twitter"}/>
-                  </Link>
-                  <Link to='https://linkedin.com/in/rebecca-palmore' target='_blank'><img className='socialIcons' src={linkedin} alt={"LinkedIn"}/>
-                  </Link>
-                </div>
+                  <div className='socialLinks'>
+                    <Link to='https://github.com/rpalmore' target='_blank'>
+                      <img className='socialIcons' src={github} alt={"Github"}/>
+                    </Link>
+                    <Link to='https://twitter.com/rebeccapalmore' target='_blank'>
+                      <img className='socialIcons' src={twitter} alt={"Twitter"}/>
+                    </Link>
+                    <Link to='https://linkedin.com/in/rebecca-palmore' target='_blank'>
+                      <img className='socialIcons' src={linkedin} alt={"LinkedIn"}/>
+                    </Link>
+                  </div>
+      	        </div>
       	      </div>
-      	    </div>
+            </div>
+
+            <div className='menuContainer'></div>
+            <div className='imageContainer'>
+              <Link onClick={this.handleClick} to='/about'>
+                <div className='about'>about</div>
+              </Link>
+		          <Link onClick={this.handleClick} to='/projects'>
+		            <div className='projects'>projects</div>
+	            </Link>
+	            <Link onClick={this.handleClick} to='/contact'>
+	              <div className='contact'>contact</div>
+	            </Link>
+	          </div>
+
+	          <div className='mainContainer'>
+		          <Route path="/about" component={About}/>
+		          <Route path="/projects" component={Projects}/>
+	            <Route path="/contact" component={Contact}/>
+            </div>
+
+          </div>
+          
+          <Footer />
         </div>
-
-        <div className='menuContainer'></div>
-        <div className='imageContainer'>
-          <Link onClick={this.handleClick} to='/about'>
-            <div className='about'>about</div>
-          </Link>
-		      <Link onClick={this.handleClick} to='/projects'>
-		        <div className='projects'>projects</div>
-	        </Link>
-	        <Link onClick={this.handleClick} to='/contact'>
-	          <div className='contact'>contact</div>
-	        </Link>
-	      </div>
-
-	      <div className='mainContainer'>
-		      <Route path="/about" component={About}/>
-		      <Route path="/projects" component={Projects}/>
-	        <Route path="/contact" component={Contact}/>
-        </div>
-
-      </div>
-      <Footer />
-     </div>
-    </Router>
-	);
+      </Router>
+	  );
   }
 }
 
